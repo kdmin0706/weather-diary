c6f1222c7e3dc5c5888dea83ef6a21f437c0629a
kdmin0706 <gxxk76@gmail.com>
2023-10-23 12:10:51 +0900

    commit..

--- src/main/java/zerobase/weather/service/SchedulerService.java
+++ src/main/java/zerobase/weather/service/SchedulerService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import zerobase.weather.repository.DateWeatherRepository;
 
 @Service
 @RequiredArgsConstructor
