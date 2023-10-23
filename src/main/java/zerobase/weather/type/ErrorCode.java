ca9677d3122609b43703cfac11baf28e941a7203
kdmin0706 <gxxk76@gmail.com>
2023-10-23 12:10:12 +0900

    error 관련 code 추가

---
+++ src/main/java/zerobase/weather/type/ErrorCode.java
@@ -0,0 +1,13 @@
+package zerobase.weather.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR("내부 서버 오류가 발생했습니다."),
+    INVALID_REQUEST("잘못된 요청입니다.");
+
+    private final String description;
+}
